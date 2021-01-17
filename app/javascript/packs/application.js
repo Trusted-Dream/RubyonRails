2b2da3d3d09085b09fbff14ddda3690a125f9ba0
Trusted-Dream <trusted.dream.fun@gmail.com>
2021-01-17 10:05:19 +0900

    8.2 ログイン

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("jquery")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import 'bootstrap'
+import '../src/application.scss'
